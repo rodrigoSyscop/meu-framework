3f4e7c036094f7158f8a82c45b9190e55d8a5595
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2018-02-17 13:29:41 -0200

    Torna o index.php mais seguro e "testável"

--- framework/index.php
+++ framework/index.php
@@ -1,4 +1,9 @@
 <?php
 
-$input = $_GET['name'];
-printf("Hello %s", $input);
+// framework/index.php
+$input = isset($_GET['name']) ? $_GET['name'] :  'World';
+
+header('Content-Type: text/html; charset=utf-8');
+
+printf("Hello %s", htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+
---
+++ framework/tests/indexTest.php
@@ -0,0 +1,18 @@
+<?php
+
+// framework/test.php
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+  public function testHello()
+  {
+    $_GET['name'] = 'Fabien';
+
+    ob_start();
+    include 'index.php';
+    $content = ob_get_clean();
+
+    $this->assertEquals('Hello Fabien', $content);
+  }
+}
