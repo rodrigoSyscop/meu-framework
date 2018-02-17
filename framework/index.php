4ff2a02e3a53973c45c982de07bfe2dfcc6ef7f7
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2018-02-17 15:10:37 -0200

    Etapa 2: O componente HttpFoundation

--- framework/index.php
+++ framework/index.php
@@ -1,9 +1,16 @@
 <?php
 
 // framework/index.php
-$input = isset($_GET['name']) ? $_GET['name'] :  'World';
+require_once __DIR__.'/vendor/autoload.php';
 
-header('Content-Type: text/html; charset=utf-8');
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-printf("Hello %s", htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+$request = Request::createFromGlobals();
+
+$input = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
 
