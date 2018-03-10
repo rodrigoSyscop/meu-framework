2f735db3b4b5b708947b8387078f77469f88d2f6
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2018-03-10 15:00:12 -0300

    Etapa 5 - Templating

--- framework/src/app.php
+++ framework/src/app.php
@@ -1,21 +1,26 @@
 <?php //framework/src/app.php
 
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
 
 $routes = new Routing\RouteCollection();
-$routes->add(
-    'hello',
-    new Routing\Route(
-        '/hello/{name}',
-        array('name' => 'World')
-    )
-);
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
 
-$routes->add(
-    'bye',
-    new Routing\Route(
-        'bye'
-    )
-);
+        return new Response('Nope, this is not a leap year.');
+    }
+)));
 
 return $routes;
--- framework/web/front.php
+++ framework/web/front.php
@@ -6,6 +6,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
@@ -14,14 +23,12 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
-    $response = new Response('An error ocurred', 500);
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
